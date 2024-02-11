fd5f6932b7fa821d8f13cd460de0eb6839320041
Johnny Jurak <johny9020@hotmail.com>
2024-02-11 11:57:11 +0100

    refactor: Changed File Names and moved models into separate files

---
+++ models/ApiKeyModel.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, DateTime, func
+from datetime import datetime
+from uuid import uuid4
+from database import Base
+
+
+class APIKey(Base):
+    __tablename__ = 'api_keys'
+
+    id = Column(String, primary_key=True, unique=True, index=True, default=str(uuid4()).replace('-', '_'))
+    api_key = Column(String, index=True, unique=True)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(DateTime, nullable=True, default=None, onupdate=datetime.now)
